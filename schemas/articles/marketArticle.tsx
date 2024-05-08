a2af32029718bbd8b7d56cc1bb9e89be8d10579e
dev-phantom <godsfavour1975@gmail.com>
2024-05-08 19:35:32 +0100

    completed market article

--- schemas/articles/marketArticle.tsx
+++ schemas/articles/marketArticle.tsx
@@ -1,4 +1,15 @@
-// schema.js
+import {PlayIcon} from '@sanity/icons'
+// import { YouTubePreview } from '../utils/YouTubePreview';
+const HighlightIcon = () => (
+  <span style={{ fontWeight: 'bold' }}>H</span>
+)
+const HighlightDecorator = (props : any) => (
+  <span style={{ backgroundColor: 'yellow' }}>{props.children}</span>
+)
+
+const TitleStyle = (props: { children: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | Iterable<React.ReactNode> | null | undefined; }) => (
+  <span style={{fontFamily: 'Garamond', fontSize: '2em'}}>{props.children} </span>
+)
 
 export default {
   name: 'marketArticle',
@@ -28,6 +39,7 @@ export default {
       title: 'Description',
       type: 'text',
       description: 'A brief description of the market insight blog post',
+      validation: (Rule: any) => Rule.max(500).error('Title must not be more than 500 characters long.'),
     },
     {
       name: 'coverImage',
@@ -46,13 +58,19 @@ export default {
         {
           type: 'block',
           styles: [
-            { title: 'Normal', value: 'normal' },
+            { title: 'Normal', value: 'normal', },
             { title: 'Heading 1', value: 'h1' },
             { title: 'Heading 2', value: 'h2' },
             { title: 'Heading 3', value: 'h3' },
             { title: 'Heading 4', value: 'h4' },
             { title: 'Heading 5', value: 'h5' },
-            { title: 'Heading 6', value: 'h6' }
+            { title: 'Heading 6', value: 'h6' },
+            {title: 'Quote', value: 'blockquote'},
+            {
+              title: 'Title',
+              value: 'title',
+              component: TitleStyle
+            },
           ],
           lists: [
             { title: 'Bullet', value: 'bullet' },
@@ -65,8 +83,17 @@ export default {
               { title: 'Underline', value: 'underline' },
               { title: 'Strike', value: 'strike-through' },
               { title: 'Code', value: 'code' },
-            ]
-          }
+              {
+                title: 'Highlight',
+                value: 'highlight',
+                icon: HighlightIcon,
+                component: HighlightDecorator
+              }
+            ],
+           
+          },
+          
+
         },        
         {
           type: 'image',
@@ -76,7 +103,27 @@ export default {
           ],
           options: { hotspot: true },
         },
-       
+        {
+          name: 'youtube',
+          type: 'object',
+          title: 'YouTube Embed',
+          icon: PlayIcon,
+          fields: [
+            {
+              name: 'url',
+              type: 'url',
+              title: 'YouTube video URL',
+
+            },
+          ],
+          // preview: {
+          //   select: {title: 'url'},
+
+          // },
+          // components: {
+          //   preview: YouTubePreview,
+          // },
+        }        
       ],
       description: 'The content of the market insight blog post',
     },
