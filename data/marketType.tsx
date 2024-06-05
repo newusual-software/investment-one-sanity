7cdd195270f4e2f7229c742cde4a711e4d2bf1a2
dev-phantom <godsfavour1975@gmail.com>
2024-06-05 03:25:40 +0100

    fixed market type

---
+++ data/marketType.tsx
@@ -0,0 +1,6 @@
+export const marketTypes = [
+  {title: 'Fiscal', value: 'Fiscal'},
+  {title: 'Fixed Income', value: 'Fixed Income'},
+  {title: 'Equity', value: 'Equity'},
+  {title: 'Foreign Exchange', value: 'Foreign Exchange'},
+]
